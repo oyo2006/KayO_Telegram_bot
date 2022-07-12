a833c8b1e83a6ca7cc75fbd1ae276561920247e8
oyo2006 <omranmansour097@gmail.com>
2022-07-12 15:02:40 +0200

    remove some unused stuff

--- info.py
+++ info.py
@@ -1,8 +1,5 @@
 import telebot
 import time
 from telebot import types
-from telebot.handler_backends import State, StatesGroup
-from telebot.storage import StateMemoryStorage
-tok = '5216980167:AAGJ7y5qXW8VxVej9s0X7kuHty2qtJbGAN4'
-state_storage = StateMemoryStorage()
-bot = telebot.TeleBot(tok, state_storage=state_storage)
+tok = 'Your token goes here'
+bot = telebot.TeleBot(tok)
